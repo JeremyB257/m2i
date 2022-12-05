e54cdf9af32490503fdcd9c53c36f4ad31a9d0e5
Jerem <bertelootjerem@gmail.com>
2022-12-05 15:41:32 +0100

    MAJ script

---
+++ Javascript/01-Base/script.js
@@ -0,0 +1,11 @@
+// On peut declarer des varaibles en Js
+let firstname = 'Fiorella';
+
+console.log('Salut ' + firstname);
+
+// popup navigateur
+// alert('alerte Navigateur');
+
+// Demander dse choses a l'utilisateur
+//let username = prompt('Quel est ton nom');
+//console.log('Tu te nommes ' + username);
