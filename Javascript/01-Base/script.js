1fcfa4fa13951c27502ff5d2c3172966a233801e
Jerem <bertelootjerem@gmail.com>
2022-12-05 16:31:21 +0100

    tableau

--- Javascript/01-Base/script.js
+++ Javascript/01-Base/script.js
@@ -9,3 +9,9 @@ console.log('Salut ' + firstname);
 // Demander dse choses a l'utilisateur
 //let username = prompt('Quel est ton nom');
 //console.log('Tu te nommes ' + username);
+
+// Creer des tableau
+
+let bag = [15, 4, 12, 'Hello world !'];
+
+console.log(bag);
