5df41346051f2077558de94cdf57bf04e9e7640c
Jerem <bertelootjerem@gmail.com>
2022-12-06 11:12:26 +0100

    boucle for

---
+++ Javascript/03-boucles/script.js
@@ -0,0 +1,19 @@
+/* Les boucles (for et while)
+le for a 3 instruction 
+-initialisation (1=0)
+-condition jusqu'a (i > 10)
+- incrementation (i++)
+*/
+/* for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// for of pour les tableaux 
+for ( var product of products) {
+
+}
+//for in pour les objets
+for (var index in products) {
+
+}
+ */
