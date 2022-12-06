47f49089b07b844ee5b810e2203e53d0a99b93b3
Jerem <bertelootjerem@gmail.com>
2022-12-06 11:21:03 +0100

    while

--- Javascript/03-boucles/script.js
+++ Javascript/03-boucles/script.js
@@ -8,12 +8,27 @@ le for a 3 instruction
   console.log(i);
 }
 
-// for of pour les tableaux 
-for ( var product of products) {
-
+// for of pour les tableaux
+for (var product of products) {
 }
 //for in pour les objets
 for (var index in products) {
+} */
 
+/* 
+//Le while
+var wheather = true;
+while (weather) {
+  console.log('Je sors');
 }
+
+//Le do while se fait au moins une fois
+var night = false;
+
+do {
+  night = confirm('Fait il nuit ?');
+  if (night) {
+    console.log('On voit rien');
+  }
+} while (night);
  */
