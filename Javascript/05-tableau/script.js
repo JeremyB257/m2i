52e2d1ca357aecf51c89f2fa04641e6bd32ac4f3
Jerem <bertelootjerem@gmail.com>
2022-12-07 15:14:21 +0100

    parcour tab

--- Javascript/05-tableau/script.js
+++ Javascript/05-tableau/script.js
@@ -1,3 +1,20 @@
 // Les tableau en JS
-
 let firstnames = ['Fiorella', 'Marina', 'Matthieu'];
+
+// parcourir un tableau avec une fonction de callback (fléché ou non)
+firstnames.forEach((firstname) => {
+  console.log(firstname);
+});
+
+// parcourir un tableau avec une boucle for ... of
+for (let firstname of firstnames) {
+  console.log(firstname);
+}
+// parcourir un tableau avec une boucle for ... in
+for (let index in firstnames) {
+  console.log(`${index}: ${firstnames[index]}`);
+}
+// parcourir un tableau avec une boucle avec index et valeur(avancée++)
+for (let [index, firstname] of firstnames.entries()) {
+  console.log(`${index}: ${firstname}`);
+}
