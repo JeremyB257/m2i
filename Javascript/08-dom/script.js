eedc21f55389b11f031d7767decae46617666c21
Jerem <bertelootjerem@gmail.com>
2022-12-12 16:58:00 +0100

    exercice 3

---
+++ Javascript/08-dom/script.js
@@ -0,0 +1,28 @@
+//exercice 1
+
+document.body.style.backgroundColor = '#d5b9b9';
+
+//exercice 2
+let para = document.getElementsByTagName('p');
+for (let i = 0; i < para.length; i++) {
+  para[i].style.fontSize = '21px';
+  para[i].innerHTML = para[i].innerHTML.replace('Lorem', '<span style="color: green">Lorem</span>');
+}
+
+//exercice 3
+
+let link = document.createElement('a');
+link.innerHTML = 'Le lien en question';
+link.href = 'https://developer.mozilla.org/';
+link.style.color = '#ffff';
+link.style.textDecoration = 'none';
+document.body.appendChild(link);
+
+function randColor() {
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let red = Math.floor(Math.random() * 255);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+console.log(randColor());
