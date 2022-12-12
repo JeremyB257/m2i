1fd38dad2926b93fdf739e2ac496e323d5f63a8c
Jerem <bertelootjerem@gmail.com>
2022-12-12 09:36:03 +0100

    exo 2

--- Javascript/07-fonction/script.js
+++ Javascript/07-fonction/script.js
@@ -1,5 +1,17 @@
+//exercice 1
 function square(n1) {
   return n1 ** 2;
 }
 
 console.log(square(5));
+
+//exercice 2
+function bitcoin(euro) {
+  if (euro.includes('€')) {
+    return (parseFloat(euro) / 16072).toFixed(8) + ' Bitcoin';
+  } else {
+    return (parseFloat(euro) * 16072).toFixed(2) + ' Euro';
+  }
+}
+console.log(bitcoin('16'));
+console.log(bitcoin('16€'));
