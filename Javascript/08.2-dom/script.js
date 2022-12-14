497370886339282ecf73e635e6dc6127ab127c64
Jerem <bertelootjerem@gmail.com>
2022-12-14 11:03:14 +0100

    MAJ

--- Javascript/08.2-dom/script.js
+++ Javascript/08.2-dom/script.js
@@ -4,13 +4,19 @@ function handleTitle(input) {
   title.innerHTML = input.value;
 }
 
-function handleClasses() {
+function handleClasses(e) {
   title.classList.toggle(this.dataset.class);
 
   if (title.classList.contains(this.dataset.class)) {
     this.innerHTML = this.innerHTML.replace('Ajouter', 'Retirer');
   } else {
     this.innerHTML = this.innerHTML.replace('Retirer', 'Ajouter');
+  }
+
+  if (this.dataset.class == 'invert') {
+    this.parentNode.children[2].click();
+    this.parentNode.children[3].click();
+    this.parentNode.children[4].click();
   }
 }
 for (let button of buttons) {
