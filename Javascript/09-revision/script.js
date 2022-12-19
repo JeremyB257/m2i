a633cc66266cddff82b3063cb55badbe8fe3bf7a
Jerem <bertelootjerem@gmail.com>
2022-12-19 10:01:23 +0100

    exo 3

--- Javascript/09-revision/script.js
+++ Javascript/09-revision/script.js
@@ -1,3 +1,4 @@
+//exo 1
 document.querySelector('ul').addEventListener('mouseenter', () => {
   let dones = document.querySelectorAll('.done');
   for (let done of dones) {
@@ -5,9 +6,21 @@ document.querySelector('ul').addEventListener('mouseenter', () => {
   }
 });
 
+//exo 2 et 3
+let count = 0;
+
 document.querySelector('h2').addEventListener('click', () => {
+  count++;
   let ongoings = document.querySelectorAll('.ongoing');
+  let comings = document.querySelectorAll('.coming');
+
   for (let ongoing of ongoings) {
     ongoing.style.backgroundColor = 'khaki';
+  }
+
+  if (count >= 3) {
+    for (let coming of comings) {
+      coming.style.backgroundColor = 'lightcoral';
+    }
   }
 });
