1c2c4e8d3929599dd640617bb39fa13d02a084e2
Jerem <bertelootjerem@gmail.com>
2022-12-20 14:03:31 +0100

    MAJ name variable

--- Javascript/09.2 revision/script.js
+++ Javascript/09.2 revision/script.js
@@ -89,18 +89,17 @@ const arabicInput = document.querySelector('.arabicInput');
 const romanInput = document.querySelector('.romanInput');
 
 romanInput.addEventListener('input', () => {
-  arabicInput.value = roman_to_Int(romanInput.value);
+  arabicInput.value = romanToInt(romanInput.value);
 });
 arabicInput.addEventListener('input', () => {
-  romanInput.value = int_to_roman(arabicInput.value);
+  romanInput.value = intToRoman(arabicInput.value);
 });
 
-function roman_to_Int(str1) {
-  if (str1 == null) return -1;
-  var num = char_to_int(str1.charAt(0));
-  var pre, curr;
+function romanToInt(str1) {
+  let num = char_to_int(str1.charAt(0));
+  let pre, curr;
 
-  for (var i = 1; i < str1.length; i++) {
+  for (let i = 1; i < str1.length; i++) {
     curr = char_to_int(str1.charAt(i));
     pre = char_to_int(str1.charAt(i - 1));
     if (curr <= pre) {
@@ -134,8 +133,8 @@ function char_to_int(c) {
   }
 }
 
-function int_to_roman(num) {
-  var lookup = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 },
+function intToRoman(num) {
+  let lookup = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 },
     roman = '',
     i;
   for (i in lookup) {
