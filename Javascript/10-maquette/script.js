a6c60068001470d85f9ddeabd8c00b7765bff3a2
Jerem <bertelootjerem@gmail.com>
2022-12-23 10:09:57 +0100

    gift data

--- Javascript/10-maquette/script.js
+++ Javascript/10-maquette/script.js
@@ -1,7 +1,17 @@
+//DATA
+const dataGifts = [
+  { name: "Pain d'épice", price: '15 €', img: 'img/gift1.png' },
+  { name: 'Chapeau du père Noël', price: '22 €', img: 'img/gift2.png' },
+  { name: 'Sapin de Noël', price: '48 €', img: 'img/gift3.png' },
+  { name: 'Bonhomme de neige', price: '35 €', img: 'img/gift4.png' },
+  { name: 'Bâton de bonbon', price: '12 €', img: 'img/gift5.png' },
+];
+
 //Selecteur
 const darkMode = document.querySelector('.darkMode');
 const header = document.querySelector('header');
 const hearts = document.querySelectorAll('.heartDiv');
+const gifts = document.querySelector('#gift');
 
 //EventListener
 darkMode.addEventListener('click', dark);
