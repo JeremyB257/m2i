9f2b021ed82ab81533c27b349cbc38d84cf19178
Jerem <bertelootjerem@gmail.com>
2023-01-03 16:26:26 +0100

    function age

--- React/01-Base/app.jsx
+++ React/01-Base/app.jsx
@@ -1,1 +1,12 @@
-ReactDOM.render(<h1>Bonjour React</h1>, document.querySelector('#root'));
+let firstName = 'Fiorella';
+
+let age = (year) => {
+  return new Date().getFullYear() - year;
+};
+
+ReactDOM.render(
+  <h1>
+    Bonjour {firstName}, tu as {age(2019)} ans.
+  </h1>,
+  document.querySelector('#root')
+);
