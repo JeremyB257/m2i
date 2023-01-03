f18b5618f01d9dd415e3273085194af25ef42f59
Jerem <bertelootjerem@gmail.com>
2023-01-03 16:28:02 +0100

    img imprt

--- React/01-Base/app.jsx
+++ React/01-Base/app.jsx
@@ -5,8 +5,11 @@ let age = (year) => {
 };
 
 ReactDOM.render(
-  <h1>
-    Bonjour {firstName}, tu as {age(2019)} ans.
-  </h1>,
+  <>
+    <h1>
+      Bonjour {firstName}, tu as {age(2019)} ans.
+    </h1>
+    <img src="un-chaton.jpeg" alt="un chaton" />
+  </>,
   document.querySelector('#root')
 );
