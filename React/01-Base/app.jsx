d7d2f6ba17370238ceeb4af7462590f6198d68a7
Jerem <bertelootjerem@gmail.com>
2023-01-03 16:31:57 +0100

    img

--- React/01-Base/app.jsx
+++ React/01-Base/app.jsx
@@ -3,13 +3,17 @@ let firstName = 'Fiorella';
 let age = (year) => {
   return new Date().getFullYear() - year;
 };
+let person = {
+  image: 'un-chaton.jpeg',
+  name: 'Bianca',
+};
 
 ReactDOM.render(
-  <>
+  <div>
     <h1>
       Bonjour {firstName}, tu as {age(2019)} ans.
     </h1>
-    <img src="un-chaton.jpeg" alt="un chaton" />
-  </>,
+    <img src={person.image} alt={person.name} />
+  </div>,
   document.querySelector('#root')
 );
