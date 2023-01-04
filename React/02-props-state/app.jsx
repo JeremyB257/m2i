09328371a8d7af7fa4b02c1ebd465f03b9876ab1
Jerem <bertelootjerem@gmail.com>
2023-01-04 16:28:36 +0100

    exo

--- React/02-props-state/app.jsx
+++ React/02-props-state/app.jsx
@@ -1,1 +1,31 @@
-ReactDOM.render(<div></div>, document.querySelector('#root'));
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+    setInterval(() => this.setState({ date: new Date() }), 1000);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>
+          Il est {this.state.date.toLocaleTimeString('fr-FR', { timeZone: this.props.timezone })} à{' '}
+          {this.props.timezone.split('/')[1]}
+        </h2>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Clock timezone="Europe/Paris" />
+        <Clock timezone="Asia/Tokyo" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
