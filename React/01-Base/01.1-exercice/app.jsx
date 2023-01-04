fc315f16a56443a5374fe1c8565957bb08a4b7b9
Jerem <bertelootjerem@gmail.com>
2023-01-04 09:53:08 +0100

    init exo 3

--- React/01-Base/01.1-exercice/app.jsx
+++ React/01-Base/01.1-exercice/app.jsx
@@ -65,3 +65,96 @@ ReactDOM.render(
 );
 
 //exercice 3
+
+class App3 extends React.Component {
+  render() {
+    const person = {
+      name: 'Charly',
+      age: 66,
+      job: {
+        name: 'Styliste',
+        since: '29 Avril 2020',
+      },
+    };
+
+    const shoppingCart = [
+      {
+        name: 'Concombre',
+        type: 'Legume',
+      },
+      {
+        name: 'Courgette',
+        type: 'Legume',
+      },
+      {
+        name: 'Endive',
+        type: 'Beurk',
+      },
+      {
+        name: 'Steak',
+        type: 'Frais',
+      },
+      {
+        name: 'Sanglier',
+        type: null,
+      },
+    ];
+
+    const series = this.catalogSeries();
+
+    return (
+      <div>
+        <div>
+          <p>Consulter la méthode someDiv. En vous basant sur le même modèle, remplissez la méthode validateButton</p>
+          <p>Appeler la fonction validateButton pour générer le bouton en dessous de ce paragraphe</p>
+          <p>{this.validateButton()}</p>
+          <p>Afficher toutes les propriétés de la variable person</p>
+          <p>Afficher toutes les propriétés du panier de courses</p>
+          <p>Afficher toutes les propriétés du catalogue de séries</p>
+        </div>
+      </div>
+    );
+  }
+
+  /**
+   * Cette méthode renvoie une div au format JSX
+   */
+  someDiv() {
+    return <div>Je suis une div</div>;
+  }
+
+  /**
+   * Cette méthode renvoie un bouton Valider au format JSX
+   */
+  validateButton() {}
+
+  /**
+   * Cette méthode renvoie une liste des séries à regarder pendant le confinement
+   */
+  catalogSeries() {
+    return [
+      {
+        name: 'One Sheep',
+        episodes: 999999,
+        note: 5,
+      },
+      {
+        name: 'Vikongs',
+        episodes: 5,
+        note: 19,
+      },
+      {
+        name: 'Spartakis, au bon lait de brebis',
+        episodes: 15,
+        note: 12,
+      },
+    ];
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <App3 />
+  </div>,
+  document.querySelector('#root3')
+);
