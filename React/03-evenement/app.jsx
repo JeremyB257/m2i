69dd345dc85462845e1ac58f4032ad2717294e7f
Jerem <bertelootjerem@gmail.com>
2023-01-05 08:56:09 +0100

    add app + button

--- React/03-evenement/app.jsx
+++ React/03-evenement/app.jsx
@@ -1,6 +1,23 @@
+class Button extends React.Component {
+  handleClick() {
+    console.log('click');
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>click</button>
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Button />
+      </div>
+    );
   }
 }
 
