7fbfdafbc8602342d6f904117e55ac8f73860a4f
Jerem <bertelootjerem@gmail.com>
2023-01-05 13:32:18 +0100

    init state 1

--- React/03.1-exercice/app.jsx
+++ React/03.1-exercice/app.jsx
@@ -372,6 +372,35 @@ class App5 extends React.Component {
   }
 }
 
+//state 1
+
+class App6 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Matthieu',
+      cat: {
+        name: 'Bianca',
+        age: 6,
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Afficher la propriété name du state</p>
+        <p>Afficher la propriété cat du state avec ses attributs</p>
+        <div>
+          <h2>Le cat est là</h2>
+        </div>
+        <p>Créer une propriété song dans le state, donnez-lui la valeur de votre choix, et affichez-là ici.</p>
+        <p>Créer une propriété bananaNumber dans le state, donnez-lui la valeur de votre choix, et affichez-la ici.</p>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
   <div>
     <App />
