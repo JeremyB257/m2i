99367fa4bcc8d581be0093e39560828695245d12
Jerem <bertelootjerem@gmail.com>
2023-01-05 09:22:25 +0100

    add button class

--- React/03-evenement/app.jsx
+++ React/03-evenement/app.jsx
@@ -1,11 +1,19 @@
 class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggle: true };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   handleClick() {
     console.log('click');
+    this.setState({ isToggle: !this.state.isToggle });
   }
+
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>click</button>
+        <button onClick={this.handleClick}>click ({this.state.isToggle ? 'Ouvert' : 'Fermé'})</button>
       </div>
     );
   }
