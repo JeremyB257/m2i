201874ae3bde1702b7e5678c7396f75f0edf48d0
Jerem <bertelootjerem@gmail.com>
2023-01-05 10:36:43 +0100

    init exo

---
+++ React/03.1-exercice/app.jsx
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
