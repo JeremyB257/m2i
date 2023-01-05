192bdf04595e0fc39d0518cf06408e75518326e5
Jerem <bertelootjerem@gmail.com>
2023-01-05 13:44:21 +0100

    init state 3

--- React/03.1-exercice/app.jsx
+++ React/03.1-exercice/app.jsx
@@ -476,6 +476,45 @@ class App7 extends React.Component {
     this.setState({ text: 'Baba' });
   }
 }
+
+// state 3
+
+class App8 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textExample: 'baba',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <p>Voici un input qui est lié au state textExample</p>
+          <input value={this.state.textExample} onChange={this.handleChange} />
+          <p>Voici sa valeur {this.state.textExample}</p>
+        </div>
+        <div>
+          <p>Créer un input de type texte qui permettra de saisir un texte et qui aura pour valeur le texte du state</p>
+          <p>
+            Créer une méthode "handleTextChange" qui sera déclenchée à chaque fois que le texte changera. Cette méthode
+            sera appelée avec le déclencheur onChange de l'input
+          </p>
+          <p>Afficher la valeur de texte ici : {this.state.text}</p>
+        </div>
+      </div>
+    );
+  }
+
+  handleChange(event) {
+    this.setState({
+      textExample: event.target.value,
+    });
+  }
+}
 ReactDOM.render(
   <div>
     <App />
@@ -485,6 +524,7 @@ ReactDOM.render(
     <App5 />
     <App6 />
     <App7 />
+    <App8 />
   </div>,
   document.querySelector('#root')
 );
