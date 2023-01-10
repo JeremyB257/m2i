f952b1985314adaa6294289037e263619756db93
Jerem <bertelootjerem@gmail.com>
2023-01-10 12:10:55 +0100

    init exo 3/4

--- React/04.1-exercice/app.jsx
+++ React/04.1-exercice/app.jsx
@@ -121,4 +121,134 @@ class Instructions extends React.Component {
     );
   }
 }
-ReactDOM.render(<App />, document.querySelector('#root'));
+
+const ERROR_ADULT = "Désolé, c'est interdit aux adultes ici !";
+const ERROR_DEAD = "Désolé, c'est interdit aux bébés ici !";
+
+class App2 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      errorMessage: '',
+      age: 0,
+      degrees: 0,
+    };
+
+    this.incrementAge = this.incrementAge.bind(this);
+    this.incrementTemperature = this.incrementTemperature.bind(this);
+    this.decrementTemperature = this.decrementTemperature.bind(this);
+  }
+
+  /**
+   * Cette méthode est appelée à la création du composant
+   */
+  componentDidMount() {}
+
+  /**
+   * Cette méthode est appelée dès qu'un setState est effectué.
+   * PrevState contient l'objet state de la classe avant le setState
+   * Pour rappel, pour accéder à l'état courant, utiliser this.state.
+   */
+  componentDidUpdate(previousProps, previousState) {}
+
+  render() {
+    return (
+      <div>
+        <div>
+          <div>
+            <h2>Exercice 1</h2>
+            <h3>Age : {this.state.age}</h3>
+            <p>Voici un bouton qui va augmenter l'âge de 1</p>
+            <button onClick={this.incrementAge}>Age +1</button>
+          </div>
+          <Instructions2 exercice={1} />
+        </div>
+        <div>
+          <div>
+            <h2>Exercice 2</h2>
+            <h3>Degrés {this.state.degrees}</h3>
+            <p>Voici deux boutons qui permettent de changer la température</p>
+            <div>
+              <button onClick={this.incrementTemperature}>Plus chaud</button>
+              <button onClick={this.decrementTemperature}>Plus froid</button>
+            </div>
+          </div>
+          <Instructions2 exercice={2} />
+        </div>
+      </div>
+    );
+  }
+
+  /**
+   * Augmente l'âge de 1 dans le state
+   */
+  incrementAge() {
+    this.setState({
+      age: this.state.age + 1,
+    });
+  }
+
+  /**
+   * Augmente de 1 la temperature dans le state
+   */
+  incrementTemperature() {
+    this.setState({
+      degrees: this.state.degrees + 1,
+    });
+  }
+
+  /**
+   * Diminue de 1 la temperature dans le state
+   */
+  decrementTemperature() {
+    this.setState({
+      degrees: this.state.degrees - 1,
+    });
+  }
+}
+
+class Instructions2 extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.exercice === 1 && (
+          <div>
+            <p>Remplir la méthode componentDidUpdate de manière à ce que :</p>
+            <p>
+              Si le nouvel âge est supérieur ou égal à 18, alors mettre à jour le state de errorMessage pour qu'il
+              contienne le texte suivant : "Désolé, c'est interdit aux adultes ici !".
+            </p>
+            <p>
+              Si le nouvel âge est inférieur ou égal à 5, alors mettre à jour le state de errorMessage pour qu'il
+              contienne le texte suivant : "Désolé, c'est interdit aux bébés ici!"
+            </p>
+            <p>Sinon, mettre à jour le state de errorMessage pour qu'il contienne un texte vide.</p>
+            <p>Remplir la méthode componentDidMount pour que le message apparaisse au chargement de la page.</p>
+          </div>
+        )}
+        {this.props.exercice === 2 && (
+          <div>
+            <p>
+              Remplir la méthode componentDidUpdate de manière à ce que lorsque la température passe en dessous de 0, la
+              console affiche "Trop froid !" et que quand la température passe au dessus de 0, la console affiche "Trop
+              chaud !"
+            </p>
+            <p>S'il fait déjà trop chaud quand on incrémente la température, ne rien afficher</p>
+            <p>S'il fait déjà trop froid quand on décrémente la température, ne rien afficher</p>
+            <p>
+              Vous pouvez manipuler prevState pour connaître l'ancienne température et la comparer avec la nouvelle.
+            </p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+ReactDOM.render(
+  <div>
+    <App />;
+    <App2 />
+  </div>,
+  document.querySelector('#root')
+);
