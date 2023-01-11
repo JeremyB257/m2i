ed9f01c8eb16cc2fb7888413d84e9e0f2c4fae2b
Jerem <bertelootjerem@gmail.com>
2023-01-11 10:10:06 +0100

    init react app

---
+++ React/05-cra/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Button from './components/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>app</h1>
+        <Button />
+      </>
+    );
+  }
+}
+
+export default App;
