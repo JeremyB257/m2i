5c59d36bd46c57aa24c1e8385348d898a15b46fb
Jerem <bertelootjerem@gmail.com>
2023-01-11 14:49:25 +0100

    clock

--- React/05-cra/src/components/Clock.jsx
+++ React/05-cra/src/components/Clock.jsx
@@ -4,15 +4,29 @@ import { useEffect } from 'react';
 
 const Clock = () => {
   const [date, setDate] = useState(new Date());
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    console.log('ici');
+    console.log('componentDidMount et componentDidUpdate');
+
+    let timer = setInterval(() => {
+      let date = new Date();
+      setDate(date);
+      date.getSeconds() % 10 === 0 ? setMessage('Ding Dong') : setMessage('');
+    }, 1000);
+
+    return () => {
+      console.log('componentwillUnmount');
+      clearInterval(timer);
+    };
   }, []);
 
   return (
     <>
       <h1>clock</h1>
-      <h1 onClick={() => setDate(new Date())}>{date.toLocaleTimeString()}</h1>
+      <h1 onClick={() => setDate(new Date())}>
+        {date.toLocaleTimeString()} {message}
+      </h1>
     </>
   );
 };
