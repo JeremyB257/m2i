21016d70df20e62936fce450f707f891dc3a5d9e
Jerem <bertelootjerem@gmail.com>
2023-01-19 11:11:04 +0100

    add link

--- React/tp-beer/src/pages/Beer.jsx
+++ React/tp-beer/src/pages/Beer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Beer = () => {
+  let params = useParams();
+
   return (
     <div>
       <h1>Beer</h1>
+      {params.id}
     </div>
   );
 };
