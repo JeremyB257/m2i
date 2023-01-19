ed6de3e757d2ee5a58edb43076439da37f8a711f
Jerem <bertelootjerem@gmail.com>
2023-01-19 13:58:34 +0100

    maj

--- React/tp-beer/src/components/Header.jsx
+++ React/tp-beer/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="header">
-      <h1>Beer App!</h1>
+      <Link to={'/'}>
+        <h1>Beer App!</h1>
+      </Link>
     </div>
   );
 };
