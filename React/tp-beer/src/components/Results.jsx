5f2b9ce6798de1f343163f969317c794cd1575e6
Jerem <bertelootjerem@gmail.com>
2023-01-19 11:05:40 +0100

    add link

--- React/tp-beer/src/components/Results.jsx
+++ React/tp-beer/src/components/Results.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Results = (props) => {
   console.log(props.data);
   return (
     <div className="results">
       {props.data.map((beer) => (
-        <div className="card">
-          <p>{beer.name}</p>
-          <img src={beer.image_url} alt={beer.name} />
-        </div>
+        <Link to={`/biere/${beer.id}/${beer.name.toLowerCase().replaceAll(' ', '-')}`}>
+          <div className="card">
+            <p>{beer.name}</p>
+            <img src={beer.image_url} alt={beer.name} />
+          </div>
+        </Link>
       ))}
     </div>
   );
