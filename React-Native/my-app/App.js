2c5d48dccd495e4def0c09621fabb3c6a0093c26
Jerem <bertelootjerem@gmail.com>
2023-01-24 13:47:16 +0100

    exo 1

--- React-Native/my-app/App.js
+++ React-Native/my-app/App.js
@@ -1,23 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import styles from './style';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={[styles.title, styles.text]}>Le titre</Text>
+          <Text style={styles.text}>Open up App.js to start working !</Text>
+          <Image source={require('./assets/favicon.png')} style={{ width: 100, height: 100 }} />
+          <Image
+            source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/130.png' }}
+            style={{ width: 100, height: 100 }}
+          />
+          <Button title="bonjour" onPress={() => alert('Salut lequipe')} color="#f194ff" />
+          <TouchableHighlight underlayColor="red" style={styles.btn} onPress={() => alert('Mon bouton')}>
+            <Text style={{ color: 'white' }}>Mon bouton</Text>
+          </TouchableHighlight>
+          <View
+            style={{
+              width: 100,
+              height: 100,
+              backgroundColor: '#7de1fa',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text>Hello World !</Text>
+          </View>
+        </View>
+        {/*  <StatusBar style="auto" /> */}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'lightcoral',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'blue',
-  },
-});
---
+++ React-Native/my-app/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'blue',
+  },
+  title: {
+    fontSize: 64,
+  },
+  btn: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+});
+
+export default styles;
