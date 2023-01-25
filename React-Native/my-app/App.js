6db8f01b7c0049f08e9d194aed3af2f2fe41e24d
Jerem <bertelootjerem@gmail.com>
2023-01-25 11:01:39 +0100

    Header

--- React-Native/my-app/App.js
+++ React-Native/my-app/App.js
@@ -2,13 +2,33 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import Exercice1 from './screens/Exercice1';
 import Home from './screens/Home';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          //tabBarLabel: 'toto',
+          //tabBarLabel: () => null,
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center',
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = 'home';
+
+            if (route.name === 'Exercice 1') {
+              iconName = 'planet';
+            }
+            iconName += focused ? '' : '-outline';
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Exercice 1" component={Exercice1} options={{ tabBarBadge: 2 }} />
       </Tab.Navigator>
