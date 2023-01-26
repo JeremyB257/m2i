b92958c140ea9dcc166df0ab46843a6221819b23
Jerem <bertelootjerem@gmail.com>
2023-01-26 14:59:32 +0100

    exo 1

--- Javascript/11-exo/script.js
+++ Javascript/11-exo/script.js
@@ -1,7 +1,7 @@
-let firstnames = ['a', 'b', 'c'];
+let firstnames = ['alpha', 'bravo', 'charlie', 'echo', 'foxstrot'];
 
 const searchInArray = (array, search) => {
-  return array.filter((item) => item.includes(search));
+  return array.filter((item) => item.includes(search)).map((name) => name.toUpperCase());
 };
 
 console.log(searchInArray(firstnames, 'a'));
