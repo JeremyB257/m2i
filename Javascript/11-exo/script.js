74b677eeefacc71678af451c1ca2010f0067227b
Jerem <bertelootjerem@gmail.com>
2023-01-27 09:05:02 +0100

    add btn three and four

--- Javascript/11-exo/script.js
+++ Javascript/11-exo/script.js
@@ -57,31 +57,40 @@ let cookieCountPara = document.querySelector('.cookie-count-p');
 let cookieImg = document.querySelector('.cookie-img');
 let btnOne = document.querySelector('.btn1');
 let btnTwo = document.querySelector('.btn2');
+let btnThree = document.querySelector('.btn3');
+let btnFour = document.querySelector('.btn4');
 let btnOnePrice = document.querySelector('.btnOnePrice');
 let btnTwoPrice = document.querySelector('.btnTwoPrice');
+let btnThreePrice = document.querySelector('.btnThreePrice');
+let btnFourPrice = document.querySelector('.btnFourPrice');
 
 let cookieCount = 0;
 let click = 1;
+let cBySecond = 0;
 let priceBtn1 = 25;
 let priceBtn2 = 250;
+let priceBtn3 = 50;
+let priceBtn4 = 300;
 
-cookieCountPara.innerHTML = cookieCount + `(${click})`;
+cookieCountPara.innerHTML = cookieCount + `(+${click} / +${cBySecond} s)`;
 
 cookieImg.addEventListener('click', cookieClick);
 btnOne.addEventListener('click', buttonOne);
 btnTwo.addEventListener('click', buttonTwo);
+btnThree.addEventListener('click', buttonThree);
+btnFour.addEventListener('click', buttonFour);
 
 function cookieClick() {
   cookieCount += click;
-  cookieCountPara.innerHTML = cookieCount + `(${click})`;
+  cookieCountPara.innerHTML = cookieCount.toLocaleString() + `(+${click} / +${cBySecond} s)`;
 }
 
 function buttonOne() {
   if (cookieCount >= priceBtn1) {
     cookieCount -= priceBtn1;
     click++;
-    priceBtn1 = Math.round(priceBtn1 * 2.5);
-    cookieCountPara.innerHTML = cookieCount + `(${click})`;
+    priceBtn1 = Math.round(priceBtn1 * 1.2);
+    cookieCountPara.innerHTML = cookieCount.toLocaleString() + `(+${click} / +${cBySecond} s)`;
     btnOnePrice.innerHTML = `prix : ${priceBtn1}`;
   }
 }
@@ -90,8 +99,33 @@ function buttonTwo() {
   if (cookieCount >= priceBtn2) {
     cookieCount -= priceBtn2;
     click += 5;
-    priceBtn2 = Math.round(priceBtn2 * 2.5);
-    cookieCountPara.innerHTML = cookieCount + `(${click})`;
+    priceBtn2 = Math.round(priceBtn2 * 1.3);
+    cookieCountPara.innerHTML = cookieCount.toLocaleString() + `(+${click} / +${cBySecond} s)`;
     btnTwoPrice.innerHTML = `prix : ${priceBtn2}`;
   }
 }
+
+function buttonThree() {
+  if (cookieCount >= priceBtn3) {
+    cookieCount -= priceBtn3;
+    cBySecond++;
+    priceBtn3 = Math.round(priceBtn3 * 1.5);
+    cookieCountPara.innerHTML = cookieCount.toLocaleString() + `(+${click} / +${cBySecond} s)`;
+    btnThreePrice.innerHTML = `prix : ${priceBtn3}`;
+  }
+}
+
+function buttonFour() {
+  if (cookieCount >= priceBtn4) {
+    cookieCount -= priceBtn4;
+    cBySecond += 5;
+    priceBtn4 = Math.round(priceBtn4 * 1.7);
+    cookieCountPara.innerHTML = cookieCount.toLocaleString() + `(+${click} / +${cBySecond} s)`;
+    btnFourPrice.innerHTML = `prix : ${priceBtn4}`;
+  }
+}
+
+let interval = setInterval(() => {
+  cookieCount += cBySecond;
+  cookieCountPara.innerHTML = cookieCount.toLocaleString() + `(+${click} / +${cBySecond} s)`;
+}, 1000);
