c2890819bc0dd63457e170e971713210171177fa
Jerem <bertelootjerem@gmail.com>
2023-01-30 11:26:55 +0100

    set title

--- React-Native/my-app/components/Item.js
+++ React-Native/my-app/components/Item.js
@@ -1,6 +1,12 @@
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback } from 'react';
 import { View, Text } from 'react-native';
 
-const Item = ({ route }) => {
+const Item = ({ route, navigation }) => {
+  useFocusEffect(() => {
+    navigation.setOptions({ title: `Item ${route.params.number}` });
+  });
+
   return (
     <View>
       <Text style={{ fontSize: 32 }}>item {route.params.number}</Text>
--- React-Native/my-app/screens/List.js
+++ React-Native/my-app/screens/List.js
@@ -2,12 +2,16 @@ import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const List = ({ navigation }) => {
   const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const goTo = (item) => {
+    navigation.navigate('Item', { number: item });
+  };
   return (
     <ScrollView>
       {items.map((item, index) => (
         <View style={styles.item} key={index}>
           <Text>
-            <Button title={item + ''} onPress={() => navigation.navigate('Item', { number: item })}></Button>
+            <Button title={item + ''} onPress={() => goTo(item)}></Button>
           </Text>
         </View>
       ))}
