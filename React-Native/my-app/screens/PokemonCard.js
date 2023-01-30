57b3db07aeffb1895a3209d13c6b36c7594dfab3
Jerem <bertelootjerem@gmail.com>
2023-01-30 15:58:58 +0100

    add stats + type

--- React-Native/my-app/screens/PokemonCard.js
+++ React-Native/my-app/screens/PokemonCard.js
@@ -1,5 +1,5 @@
 import { useFocusEffect } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 const PokemonCard = ({ route, navigation }) => {
   let pokemon = route.params.pokemon;
@@ -8,7 +8,32 @@ const PokemonCard = ({ route, navigation }) => {
   });
   return (
     <View style={styles.card}>
-      <Text>{pokemon.name}</Text>
+      <Text style={styles.title}>
+        {pokemon.name}({pokemon.id})
+      </Text>
+      <Image source={{ uri: pokemon.image }} style={{ width: 250, height: 250 }} />
+
+      {/* stats */}
+      <View style={{ borderWidth: 1, borderColor: 'lightgrey', padding: 20 }}>
+        <Text style={[styles.stats, { color: pokemon.stats.HP > 70 ? 'green' : 'red' }]}>
+          - Hp : {pokemon.stats.HP}
+        </Text>
+        <Text style={styles.stats}>- Attaque : {pokemon.stats.attack}</Text>
+        <Text style={styles.stats}>- Defense : {pokemon.stats.defense}</Text>
+      </View>
+
+      {/* type */}
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        {pokemon.apiTypes.map((type, index) => (
+          <>
+            <Text key={index}>{type.name}</Text>
+            <Image source={{ uri: type.image }} style={{ width: 20, height: 20 }} />
+          </>
+        ))}
+      </View>
+
+      {/* evolution */}
+      <View></View>
     </View>
   );
 };
@@ -25,5 +50,14 @@ const styles = StyleSheet.create({
     shadowOpacity: 1,
     shadowOffset: { height: 2 },
     elevation: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  stats: {
+    fontSize: 24,
   },
 });
