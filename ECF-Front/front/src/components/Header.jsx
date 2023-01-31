fb66e2c99e021a45e94c9c5bf9cbd3260bf994f9
Jerem <bertelootjerem@gmail.com>
2023-01-31 10:09:23 +0100

    add header

--- ECF-Front/front/src/components/Header.jsx
+++ ECF-Front/front/src/components/Header.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import logo from '../img/logo.svg';
 
 const Header = () => {
   return (
-    <div>
-      <h1>header</h1>
-    </div>
+    <header>
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+        <h1>Thaito</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">A propos</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Prix</a>
+          </li>
+          <li>
+            <a href="#">Nous contacter</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">Connexion</a>
+          </li>
+          <li className="active">
+            <a href="#">Inscription</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
