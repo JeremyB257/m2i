caff909844fb7994a429e7b3e0d2075ded87db79
Jerem <bertelootjerem@gmail.com>
2023-01-31 17:04:44 +0100

    add menu page (init)

---
+++ ECF-Front/front/src/pages/Menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <main className="menu">
+      <h1>
+        Decouvrez notre <span>menu.</span>
+      </h1>
+      <div className="cards"></div>
+    </main>
+  );
+};
+
+export default Menu;
