b27fa1fdcb95a1227639035c37b1a494d534baa3
Jerem <bertelootjerem@gmail.com>
2023-01-31 13:51:51 +0100

    add Js

--- ECF-Front/front/src/components/EatToHome.jsx
+++ ECF-Front/front/src/components/EatToHome.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import image from '../img/image-1.jpeg';
 import svgDot from '../img/dot-pattern.svg';
+import { useState } from 'react';
 
 const EatToHome = () => {
+  const [order, setOrder] = useState(false);
+  const [meet, setMeet] = useState(false);
+
+  const handleOrder = () => {
+    setOrder(true);
+    setTimeout(() => {
+      setOrder(false);
+    }, 2000);
+  };
+
+  const handleMeet = () => {
+    setMeet(!meet);
+  };
+
   return (
     <section id="eatToHome" className="eatToHome">
-      <div className="desc">
+      <p className={order ? 'order success' : 'hidden'}>Votre commande a été prise en compte</p>
+      <div className={order ? 'hidden' : 'desc'}>
         <h2>Délicieux & abordable</h2>
         <h2>Repas chez vous.</h2>
         <p>
@@ -13,16 +29,25 @@ const EatToHome = () => {
           tempora odit error.
         </p>
         <div className="button">
-          <a href="#eatToHome" className="active">
+          <a href="#" className="active" onClick={handleOrder}>
             Commander maintenant
           </a>
-          <a href="#eatToHome">
+          <a href="#" onClick={handleMeet}>
             <i className="fa-regular fa-circle-play"></i> Rencontrer les chefs
           </a>
         </div>
       </div>
-      <div className="img">
-        <img className="principal" src={image} alt="plats" />
+      <div className={order ? 'hidden' : 'img'}>
+        <img className={meet ? 'hidden' : 'principal'} src={image} alt="plats" />
+        <iframe
+          className={meet ? '' : 'hidden'}
+          width="460"
+          height="306"
+          src="https://www.youtube.com/embed/_GuOjXYl5ew"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen></iframe>
         <img className="dot" src={svgDot} alt="decoration" />
       </div>
     </section>
