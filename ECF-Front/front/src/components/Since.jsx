5a0f9374b23322d4964866335bfba3ab9e103da4
Jerem <bertelootjerem@gmail.com>
2023-01-31 14:00:33 +0100

    add Js

--- ECF-Front/front/src/components/Since.jsx
+++ ECF-Front/front/src/components/Since.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import image from '../img/image-2.jpeg';
 import svgDot from '../img/dot-pattern.svg';
+
 const Since = () => {
+  const [year, setYear] = useState(5);
+
   return (
     <section id="since" className="since">
       <div className="img">
@@ -11,16 +14,16 @@ const Since = () => {
       <div className="desc">
         <p className="primary-color">Installé depuis 2014</p>
         <h2>Nous vous servons</h2>
-        <h2>depuis 5 ans.</h2>
+        <h2>depuis {year} ans.</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint iste nulla, dolore facilis perspiciatis soluta
           tempora odit error.
         </p>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, deserunt!</p>
 
-        <a href="#since" className="active">
+        <button className="active" onClick={() => setYear(year + 1)}>
           Ajouter 1 an
-        </a>
+        </button>
       </div>
     </section>
   );
